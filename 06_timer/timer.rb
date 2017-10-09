8fcfd5dcffcc4e4fad216b7b593dc8747239abde
toptester <json900064@gmail.com>
2017-10-09 11:25:01 -0400

    Finished assignments

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,20 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+ 
+  def time_string
+    hours = @seconds/3600
+    left = @seconds - hours*3600
+    minutes = left/60
+    secondsleft = left - minutes*60
+    
+    sprintf("%02d:%02d:%02d", hours, minutes, secondsleft)
+     
+  end
+ 
+
 end
